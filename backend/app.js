27ef783c1beee5247231f65df37a10e75d0a5c7a
Pedro Henrique da Costa <pedrocostamelo.henrique91@gmail.com>
2026-09-02 17:13:28 -0300

    a

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,50 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require('express');
-const userRouter = require('./routers/users');
-const cardRouter = require('./routers/cards');
-const { login, createUser } = require('./controllers/users');
+const express = require("express");
+const userRouter = require("./routers/users");
+const cardRouter = require("./routers/cards");
+const { login, createUser } = require("./controllers/users");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const auth = require("./middlewares/auth");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const cors = require("cors");
+
 const app = express();
 const PORT = 3000;
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const auth = require("./middlewares/auth");
-const { requestLogger, errorLogger } = require("./middlewares/logger");;
-const cors = require('cors');
 
 mongoose
-    .connect('mongodb://localhost:27017/newsexplorer')
-    .then(() => console.log("Conectado ao MongoDB"))
-    .catch((err) => console.log("Erro ao conectar:", err));
+  .connect("mongodb://localhost:27017/newsexplorer")
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.log("Erro ao conectar:", err));
 
-app.use(express.json())
 const allowedOrigins = [
   "http://localhost:5173",
-  'https://newsdomain.chickenkiller.com'
+  "https://newsdomain.chickenkiller.com",
 ];
-app.use(cors({ origin: allowedOrigins }));
 
-app.use(requestLogger)
-app.post('/signin', login);
-app.post('/signup', createUser);
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log("ORIGIN RECEBIDO:", origin);
 
-app.use(auth)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origem não permitida pelo CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use(requestLogger);
+
+app.post("/signin", login);
+app.post("/signup", createUser);
+
+app.use(auth);
+
 app.use("/users", userRouter);
 app.use("/cards", cardRouter);
 
